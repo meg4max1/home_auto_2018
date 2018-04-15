87447e1fb3862a1ec9e8a9ac9975d85a2d75f6bb
maxi <meg4max1@gmail.com>
2018-04-16 00:15:15 +0200

    rpisensorsmasterrx.cpp: commented and cleaned up the code

--- rpisensorhub/rpisensorsmasterrx.cpp
+++ rpisensorhub/rpisensorsmasterrx.cpp
@@ -1,27 +1,44 @@
-#include "RF24Mesh/RF24Mesh.h"
+#include "RF24Mesh/RF24Mesh.h"  
 #include <RF24/RF24.h>
 #include <RF24Network/RF24Network.h>
-
-RF24 radio( 22, 0, BCM2835_SPI_SPEED_8MHZ);
+RF24 radio( 22, 0, BCM2835_SPI_SPEED_8MHZ);  
 RF24Network network(radio);
 RF24Mesh mesh(radio,network);
 
+const uint8_t channel = 0x40;	//nrf24l01 type channel of the mesh network
+
 int main(int argc, char** argv) {
-
-  // Set the nodeID to 0 for the master node
-  mesh.setNodeID(0);
-  mesh.begin();
   
+	mesh.setNodeID(0);	// Set the nodeID to 0 for the master node
 
-  while(1){
-	  mesh.update();
-	  
-	  mesh.DHCP();
-	  
-	    while(network.available()){
-
-		RF24NetworkHeader header;
-		network.peek(header);
-		
-			switch(header.type){
-			
+	printf("start\n");
+	mesh.begin(channel,RF24_250KBPS,60000);		// Connect to the mesh, connection speed 250kbps
+	radio.printDetails();	//print Radio Settings,for debugging
+while(1) {
+  
+    mesh.update();	// Call network.update as usual to keep the network updated
+	mesh.DHCP();	// In addition, keep the 'DHCP service' running on the master node so addresses will be assigned to the sensor nodes
+  
+  while(network.available()){	// Check for incoming data from the sensors
+    RF24NetworkHeader header;
+    network.peek(header);
+    String buf;
+    switch(header.type){	//check wich kind of message was recieved
+    
+		case 'V': network.read(header,&buf,500);	//Recieved value from sensor node
+                printf("Recieved V type header");
+                break;
+		case 'R': network.read(header,&buf,64); 	//Recieved data request from actuator or display node
+                printf("Recieved R type header");
+                break;		 
+		default:  network.read(header,0,0); 	//unknown meassage type
+                printf("Recieved unknown message type"); 
+                break;
+    }
+  }
+delay(2);
+  }
+return 0;
+}
+      
+      
