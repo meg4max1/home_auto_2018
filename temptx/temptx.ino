b1f79f24e7a255f2b23937046a5ad5eec6a4f9c2
maxi <meg4max1@gmail.com>
2018-04-17 21:54:06 +0200

    moved back to arrays

--- temptx/temptx.ino
+++ temptx/temptx.ino
@@ -13,18 +13,18 @@
 #define ONE_WIRE_BUS 2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature tempSensors(&oneWire);
-double temp[2] = {-99,-99};
+
+#define NUMBER_OF_SENSORS 4
 
 Adafruit_AM2320 am2320 = Adafruit_AM2320();
 
-struct sensor[4]{
-  String Name;
-  double Value;
-  };
 
-//sensor mySensors[4];
-
-sensor.Name = {"teich_airtemp_dallas","teich_watertemp","teich_airtemp_am2320","teich_relativeHumidity"};
+String sensorName[NUMBER_OF_SENSORS] = 
+                       {"teich_airtemp_dallas",
+                        "teich_watertemp",
+                        "teich_airtemp_am2320",
+                        "teich_relativeHumidity"};
+double sensorValue[NUMBER_OF_SENSORS];
 
 RF24 radio(7,8);
 RF24Network network(radio);
@@ -51,24 +51,26 @@ void setup() {
 
 String getJSON() {
   String jsonObject = "{\n\"node\":\"teichNode\",\n\"data\":[\n";
-  for(int i = 0; i < sizeof(temp)/sizeof(*temp); i++){
-    jsonObject = jsonObject + "{\"name\":\"temp" + i + "\",\n\"value\":" + temp[i] + "\n},\n";
+  for(int i = 0; i < NUMBER_OF_SENSORS; i++){
+    jsonObject = jsonObject + "{\"name\":\"" + sensorName[i] + "\",\n\"value\":" + sensorValue[i] + "\n},\n";
   }
   jsonObject = jsonObject + "],\n}";
   return jsonObject;
+}
+
+void getSensorVals(){
+    tempSensors.requestTemperatures();  //Request new temp readings from the dallas temperature sensors (ds18b20)
+    sensorValue[1] = tempSensors.getTempCByIndex(0);
+    sensorValue[2] = tempSensors.getTempCByIndex(1);
+    sensorValue[3] = am2320.readTemperature();
+    sensorValue[4] = am2320.readHumidity();
 }
 
 void loop() {
   mesh.update();
   
   if((now = millis()) >= (lastsent + interval)){
-    tempSensors.requestTemperatures();  //Request new temp readings from the dallas temperature sensors (ds18b20)
-    
-    sensor[1].Value = tempSensors.getTempCByIndex(0);
-    sensor[2].Value = tempSensors.getTempCByIndex(1);
-    sensor[3].Value = am2320.readTemperature();
-    sensor[4].Value = am2320.readHumidity();
-    
+    getSensorVals();
     String jsonObject = getJSON();  //build a new JSON String with new sensor values
     Serial.print(jsonObject);
     mesh.write( &jsonObject , 'V', sizeof(jsonObject)); // send a "value" Type Message containing the jsonObject
